fd2686564890aca445dc6dfaf153628351233e8c
Vinicius Silva <viniciuscassianosilva13@gmail.com>
2026-03-22 22:00:59 -0300

    inicio dos JSX

---
+++ frontend/src/pages/profissional.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./profisional.css";
+
+
+function Profisional() {
+  return (
+    <div className="home">
+      <h1>Profissional</h1>
+      <p>Bem-vindo à página do profissional!</p>
+    </div>
+  );
+}
+
+export default Profisional;
